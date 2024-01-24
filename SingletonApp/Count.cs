cec55275b1ff2d3990f7b39e374944b45a07899c
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-24 21:27:58 +0200

    Lazy InitializationAdd project files.

---
+++ SingletonApp/Count.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    internal class Count
+    {
+        private static Count instance = null;
+        public int count = 0;
+        private Count()
+        {
+        }
+        public static Count getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Count();
+            }
+            return instance;
+        }
+        public void increment ()
+        {
+            count++;
+        }
+    }
+}
---
+++ SingletonApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using SingletonExample;
+
+Count c1 = Count.getInstance();
+Count c2 = Count.getInstance();
+c1.increment();
+c2.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
+c1.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
+c2.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
+c1.increment();
+c2.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
