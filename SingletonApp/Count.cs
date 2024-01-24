a86daf7cb05d7aa68feb7c9e08421ae87d9984c6
Ahmed <ahmed.ismail14141414@gmail.com>
2024-01-24 21:27:58 +0200

    Add project files.

---
+++ SingletonApp/Count.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    internal class Count
+    {
+        private static Count instance = new Count();
+        public int count = 0;
+        private Count()
+        {
+        }
+        public static Count getInstance()
+        {
+            return instance;
+        }
+        public void increment ()
+        {
+            count++;
+        }
+    }
+}
---
+++ SingletonApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonExample;
+Count c1 = Count.getInstance();
+Count c2 = Count.getInstance();
+c1.increment();
+c2.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
+c1.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
+c2.increment();
+Console.WriteLine("counter 1 : " + c1.count.ToString());
+Console.WriteLine("Counter 2 : " + c2.count.ToString());
